69f667d22edcbc32bae2b6b980be772dc0b87016
Serghei Motpan <serghei.motpan@amdaris.com>
2025-02-10 16:35:59 +0200

    Added spring boot pagination POC

---
+++ springboot-web-samples/src/test/java/io/springbootsamples/SpringbootWebSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbootsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
