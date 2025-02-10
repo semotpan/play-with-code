151632e12c48d636bb35b3bbaa3201feac657c40
Serghei Motpan <semotpan@gmail.com>
2025-02-10 17:39:34 +0200

    added some additional fields to the view model

--- springboot-web-samples/src/main/java/io/springbootsamples/data/Person.java
+++ springboot-web-samples/src/main/java/io/springbootsamples/data/Person.java
@@ -1,4 +1,4 @@
 package io.springbootsamples.data;
 
-public record Person(Long id, String firstName, int age) {
+public record Person(Long id, String firstName, int age, Boolean active, Integer matchRatePercentage) {
 }
